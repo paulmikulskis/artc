02f35624fb5907ab1970570fd555f384e8d9586f
Paul Mikulskis <mikulskisp@gmail.com>
2022-03-18 21:43:03 -0400

    adding org

--- nodes/run/influx_wrapper.py
+++ nodes/run/influx_wrapper.py
@@ -16,7 +16,9 @@ class StatWriter:
         self.host = host
         self.client = InfluxDBClient(
             url=self.host, 
-            token=os.environ.get("INFLUX_NODE_KEY"))
+            token=os.environ.get("INFLUX_NODE_KEY"),
+            org='sungbean'
+          )
         self.deployment_id = os.environ.get("NODE_DEPLOYMENT_ID")
         self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
     
