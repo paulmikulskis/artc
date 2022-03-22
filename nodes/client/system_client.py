402a66893d64b61fe5db3f0a14bfd9c903ef5217
Paul Mikulskis <mikulskisp@gmail.com>
2022-03-21 21:24:16 -0400

    d

--- nodes/client/system_client.py
+++ nodes/client/system_client.py
@@ -66,7 +66,7 @@ class PiBot(SingleServerIRCBot):
     def on_pubmsg(self, c, e):
         a = e.arguments[0].split(":", 1)
         print('a args:', a)
-        print('e args', e)
+        print('e args', e.target)
         if len(a) > 1 and strings.lower(a[0]) == strings.lower(
             self.connection.get_nickname()
         ):
