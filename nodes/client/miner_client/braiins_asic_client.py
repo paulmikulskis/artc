2cb5d2b14f60ab7184ff0e9c50616bb40c5717fc
Paul Mikulskis <mikulskisp@gmail.com>
2022-03-25 11:55:37 -0400

    limp

--- nodes/client/miner_client/braiins_asic_client.py
+++ nodes/client/miner_client/braiins_asic_client.py
@@ -436,7 +436,7 @@ class BraiinsOsClient:
         sock.settimeout(self.timeout)
         try:
             sock.connect((host['ip'], host['port']))
-        except ConnectionRefusedError as e:
+        except ConnectionRefusedError or OSError as e:
             return self._format_MinerAPIResponse('E', 'unable to reach miner', 404)
         log.info('sending "{}" to {}'.format(command, host['connect_string']))
         sock.sendall(bytes(command, 'utf-8'))
