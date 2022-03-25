8d406938ea6a88b3dc0e000cb58173d15a09ddd3
Paul Mikulskis <mikulskisp@gmail.com>
2022-03-24 21:23:50 -0400

    spe

--- nodes/client/node_client.py
+++ nodes/client/node_client.py
@@ -150,9 +150,12 @@ def statloop(influx_stat_writer: InfluxStatWriter, braiins: BraiinsOsClient, irc
     influx_stat_writer.write_dict('test', stats)    
     irc_connection.privmsg('#'+irc_connection.nickname, 'stats::'+str(stats))
     is_mining = braiins.is_mining()
-    temps = braiins.get_temperature_list()[0] # assuming there is no error atm
-    for k, v in temps.items():
-            temps[k] = {**{'board_'+str(d[2]): {'board': d[0], 'chip': d[1]} for d in v}, 'mining': is_mining.get(k) or 'UNKNOWN'}
+    temps = braiins.get_temperature_list()
+    if temps[1]:
+        temps = temps[1]
+    else:
+        for k, v in temps.items():
+                temps[k] = {**{'board_'+str(d[2]): {'board': d[0], 'chip': d[1]} for d in v}, 'mining': is_mining.get(k) or 'UNKNOWN'}
     irc_connection.privmsg('#'+irc_connection.nickname, 'miner::'+str(temps))
 
 
