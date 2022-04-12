99f073a3779dd4821586c766304fea265ba937f6
Paul Mikulskis <mikulskisp@gmail.com>
2022-04-11 23:01:31 -0400

    bos

--- nodes/client/node_client.py
+++ nodes/client/node_client.py
@@ -175,8 +175,8 @@ def statloop(influx_stat_writer: InfluxStatWriter, braiins: BraiinsOsClient, irc
     log.debug('stats successfully written to InfluxDB')
     try:
         stats = json.dumps(stats)
-        log.debug('wrote stats:', json.dumps(stats))
-        irc_connection.privmsg('#'+irc_connection.nickname, 'stats::'+json.dumps(stats))
+        log.debug('wrote stats:', stats)
+        irc_connection.privmsg('#'+irc_connection.nickname, 'stats::'+stats)
     except:
         log.error('unable to jsonify stats received by stat_map functiong, skipping IRC communications!')
     
