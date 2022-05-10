58c213221799b079781325f8f963a02841ce7622
Paul Mikulskis <mikulskisp@gmail.com>
2022-05-10 09:52:07 -0400

    d

--- nodes/client/miner_client/braiins_asic_client.py
+++ nodes/client/miner_client/braiins_asic_client.py
@@ -184,6 +184,7 @@ class BraiinsOsClient:
         for host, port_tuple in self.connection_dict.items():
             try:
                 info = socket.gethostbyaddr(host)
+                print('!!!!got host info', info)
                 new_host_info = {
                     'url': host,
                     'hostname': info[0],
