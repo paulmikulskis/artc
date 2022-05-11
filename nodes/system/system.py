23835c6fca4541f6295991f6340f952cb1745d84
Paul Mikulskis <mikulskisp@gmail.com>
2022-05-11 17:15:13 -0400

    new thermistors

--- nodes/system/system.py
+++ nodes/system/system.py
@@ -11,18 +11,20 @@ from system.device import RelaySwitch, HallEffectFlowSensor, OneWireThermister
 from digitalio import DigitalInOut, Direction
 import board
 
-
+THERMISTOR_ADDRESSES = {
+    1: '3c9bf648ac5d',
+    2: '3c57f64872a8',
+    3: '3c95f648bddd',
+    4: '3c4bf6486f79',
+    5: '3c5bf6482baa',
+    6: '3c01f09681ea',
+    7: '3c01f0968ebe',
+    8: '3c01f096acb7',
+    9: '3c01f096c052',
+    10: '3c01f09681ea'
+}
+    
 class TacoTestSettings:
-
-    # set the hardware addresses of any thermistors used here
-    THERMISTOR_ADDRESSES = {
-        1: '3c9bf648ac5d',
-        2: '3c57f64872a8',
-        3: '3c95f648bddd',
-        4: '3c4bf6486f79',
-        5: '3c5bf6482baa',
-        6: '3c01f09681ea'
-    }
 
     # list out the devices used in the project
     # each device should have a name, and be mapped to an object
@@ -41,15 +43,6 @@ class TacoTestSettings:
 
 class JacuzziTestSettings:
     
-    # set the hardware addresses of any thermistors used here
-    THERMISTOR_ADDRESSES = {
-        1: '3c9bf648ac5d',
-        2: '3c57f64872a8',
-        3: '3c95f648bddd',
-        4: '3c4bf6486f79',
-        5: '3c5bf6482baa'
-    }
-
     # list out the devices used in the project
     # each device should have a name, and be mapped to an object
     # that extends device.Device
@@ -72,6 +65,17 @@ class JacuzziTestSettings:
     }
 
 
-THERMISTOR_ADDRESSES = TacoTestSettings.THERMISTOR_ADDRESSES
+# exported variables from whichever settings digest you want:
 device_map = TacoTestSettings.device_map
 stat_map = TacoTestSettings.stat_map
+
+
+'''
+Tubberware Controller Pinout:
+
+Taco Relay sensor:      GPIO 27, Pin 13         (purple)
+Thermistors:            GPIO 4, Pin 7           (yellow)
+Oil Pump Relay:         GPIO 17, Pin 11         (blue)
+220V Relays:            GPIO 23+24, Pin 16+18   (green+yellow)
+Flow Meter:             GPIO22, Pin 15          (white)
+'''
